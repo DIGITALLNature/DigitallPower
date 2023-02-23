a7e61edf20dce6c8bc8db5060312fab42aa12901
Simon Findling <simon.findling@digitall.com>
2023-02-23 14:40:16 +0100

    feat(maintenance): add new command solution-version to increment the version of a solution (#31)
    * feat(maintenance): add new command solution-version to increment the version of a solution
    
    * refactor: clean up and remove unnecessary options

--- src/dgt.power/Program.cs
+++ src/dgt.power/Program.cs
@@ -11,9 +11,6 @@ using dgt.power.codegeneration.Logic;
 using dgt.power.codegeneration.Services;
 using dgt.power.codegeneration.Services.Contracts;
 using dgt.power.common;
-using dgt.power.common.Commands;
-using dgt.power.common.Exceptions;
-using dgt.power.common.Extensions;
 using dgt.power.common.FileAccess;
 using dgt.power.common.Logic;
 using dgt.power.export.Base;
@@ -118,6 +115,9 @@ app.Configure(config =>
                 .WithDescription(
                     "Exports all active carriers from an environment to a json file. To see what an carrier is check [link]https://dev.azure.com/ec4u/Dynamics%20DevLab/_wiki/wikis/Dynamics-DevLab.wiki/111/Solution-Concept[/]")
                 .WithExample(new[] {"maintenance", "carrierinfo", "--filedir", "./carriers", "--filename", "carrier.json"});
+            maintenance.AddCommand<IncrementSolutionVersion>("solution-version")
+                .WithDescription("Increments the solution version by given flag")
+                .WithExample(new[] {"maintenance", "solution-version", "sample_solution", "--minor"});
         });
 
     config.AddBranch<AnalyzeVerb>("analyze", analyze =>
@@ -127,15 +127,15 @@ app.Configure(config =>
             .WithDescription("Scans the specified solutions for entities containing with assets");
         analyze.AddCommand<NoActiveLayerAnalyze>("noactivelayer")
             .WithDescription("Scans the specified (unmanaged) solutions for components without active layer")
-            .WithExample(new[] { "analyze", "noactivelayer", "--inline", "solution1,solution2" });
+            .WithExample(new[] {"analyze", "noactivelayer", "--inline", "solution1,solution2"});
         analyze.AddCommand<RedundantComponentsAnalyze>("redundantcomponents")
             .WithDescription("Scans for components that are in multiple of the specified solutions");
         analyze.AddCommand<ActiveLayerAnalyze>("activelayer")
             .WithDescription("Scans the specified (managed) solutions for components with active layer")
-            .WithExample(new[] { "analyze", "activelayer", "--inline", "solution1,solution2" });
+            .WithExample(new[] {"analyze", "activelayer", "--inline", "solution1,solution2"});
         analyze.AddCommand<TopLayerAnalyze>("toplayer")
             .WithDescription("Scans the specified (managed) solutions for components where solution is not top layer")
-            .WithExample(new[] { "analyze", "toplayer", "--inline", "solution1,solution2" });
+            .WithExample(new[] {"analyze", "toplayer", "--inline", "solution1,solution2"});
     });
 
     config.AddBranch<ImportVerb>("import", import =>
---
+++ tests/dgt.power.maintenance.tests/IncrementSolutionVersionTests.cs
@@ -0,0 +1,189 @@
+﻿// Copyright (c) DIGITALL Nature. All rights reserved
+// DIGITALL Nature licenses this file to you under the Microsoft Public License.
+
+using dgt.power.dataverse;
+using dgt.power.maintenance.Logic;
+using dgt.power.maintenance.Model.Settings;
+using dgt.power.tests;
+using dgt.power.tests.Extensions;
+
+namespace dgt.power.maintenance.tests;
+
+public class IncrementSolutionVersionTests : CommandTestsBase<IncrementSolutionVersion, IncrementSolutionVersionSettings>
+{
+    public IncrementSolutionVersionTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+    {
+    }
+
+    [Fact]
+    public void ShouldFailOnEmptySolutionOption() => GetContext()
+        .Execute(new IncrementSolutionVersionSettings
+        {
+            Solution = string.Empty
+        }).Should()
+        .Fail();
+
+    [Fact]
+    public void ShouldFailOnNonExistingSolution() => GetContext()
+        .Execute(new IncrementSolutionVersionSettings
+        {
+            Solution = "missing"
+        }).Should()
+        .Fail();
+
+    [Fact]
+    public void ShouldFailOnInvalidSolutionVersion() => GetBuilder()
+        .WithData(new Solution(Guid.NewGuid())
+        {
+            UniqueName = "existing",
+            Version = "invalid"
+        })
+        .Build()
+        .Execute(new IncrementSolutionVersionSettings
+        {
+            Solution = "existing"
+        }).Should()
+        .Fail();
+
+    [Fact]
+    public void ShouldFailOnInvalidOptionCombination() => GetBuilder()
+        .WithData(new Solution(Guid.NewGuid())
+        {
+            UniqueName = "existing",
+            Version = "1.0.1.2"
+        })
+        .Build()
+        .Execute(new IncrementSolutionVersionSettings
+        {
+            Solution = "existing",
+            Major = false,
+            Minor = false,
+            Build = false,
+            Revision = false
+        }).Should()
+        .Fail();
+
+    [Fact]
+    public void ShouldUpdateMajorVersion()
+    {
+        var solution = new Solution(Guid.NewGuid())
+        {
+            UniqueName = "existing",
+            Version = "1.0.1.2"
+        };
+
+        var context = GetBuilder()
+            .WithData(solution)
+            .Build();
+
+        context
+            .Execute(new IncrementSolutionVersionSettings
+            {
+                Solution = solution.UniqueName,
+                Major = true
+            }).Should()
+            .Succeed();
+
+        context.GetSingle<Solution>(x => x.Id == solution.Id)
+            .Version.Should().Be("2.0.0.0");
+    }
+
+    [Fact]
+    public void ShouldUpdateMinorVersion()
+    {
+        var solution = new Solution(Guid.NewGuid())
+        {
+            UniqueName = "existing",
+            Version = "1.0.1.2"
+        };
+
+        var context = GetBuilder()
+            .WithData(solution)
+            .Build();
+
+        context
+            .Execute(new IncrementSolutionVersionSettings
+            {
+                Solution = solution.UniqueName,
+                Minor = true,
+            }).Should()
+            .Succeed();
+
+        context.GetSingle<Solution>(x => x.Id == solution.Id)
+            .Version.Should().Be("1.1.0.0");
+    }
+
+    [Fact]
+    public void ShouldUpdateBuildVersion()
+    {
+        var solution = new Solution(Guid.NewGuid())
+        {
+            UniqueName = "existing",
+            Version = "1.0.1.2"
+        };
+
+        var context = GetBuilder()
+            .WithData(solution)
+            .Build();
+
+        context
+            .Execute(new IncrementSolutionVersionSettings
+            {
+                Solution = solution.UniqueName,
+                Build = true,
+            }).Should()
+            .Succeed();
+
+        context.GetSingle<Solution>(x => x.Id == solution.Id)
+            .Version.Should().Be("1.0.2.0");
+    }
+
+    [Fact]
+    public void ShouldUpdateRevisionVersion()
+    {
+        var solution = new Solution(Guid.NewGuid())
+        {
+            UniqueName = "existing",
+            Version = "1.0.1.2"
+        };
+
+        var context = GetBuilder()
+            .WithData(solution)
+            .Build();
+
+        context
+            .Execute(new IncrementSolutionVersionSettings
+            {
+                Solution = solution.UniqueName,
+                Revision = true,
+            }).Should()
+            .Succeed();
+
+        context.GetSingle<Solution>(x => x.Id == solution.Id)
+            .Version.Should().Be("1.0.1.3");
+    }
+
+    [Fact]
+    public void ShouldUpdateRevisionIfAllFlagsOmitted()
+    {
+        var solution = new Solution(Guid.NewGuid())
+        {
+            UniqueName = "existing",
+            Version = "1.0.1.2"
+        };
+
+        var context = GetBuilder()
+            .WithData(solution)
+            .Build();
+
+        context
+            .Execute(new IncrementSolutionVersionSettings
+            {
+                Solution = solution.UniqueName,
+            }).Should()
+            .Succeed();
+
+        context.GetSingle<Solution>(x => x.Id == solution.Id)
+            .Version.Should().Be("1.0.1.3");
+    }
+}
