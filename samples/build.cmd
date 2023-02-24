5ebfb8e000a9d654b1e1aff3d27f2e6970af8b5c
Marcel Kummerow <Marcel.Kummerow@digitall.com>
2023-02-24 19:25:11 +0100

    test(push): save output for testing - allows testing in linux

--- samples/build.cmd
+++ samples/build.cmd
@@ -1,3 +1,3 @@
 dotnet build .\dgt.power.push.tests.sample\dgt.power.push.tests.sample.csproj
-copy .\dgt.power.push.tests.sample\bin\Debug\dgt.power.push.tests.sample.1.0.0.nupkg ..\artifacts\SamplePlugin\ 
-copy .\dgt.power.push.tests.sample\bin\Debug\net462\dgt.power.push.tests.sample.dll ..\artifacts\SamplePlugin\    
+copy .\dgt.power.push.tests.sample\bin\Debug\dgt.power.push.tests.sample.1.0.0.nupkg .\output\ 
+copy .\dgt.power.push.tests.sample\bin\Debug\net462\dgt.power.push.tests.sample.dll .\output\    
