076b680411443141919d815005f00df2abf79c3e
Micha Oberstein <Moberstein@users.noreply.github.com>
2023-06-27 17:46:26 +0200

    feat(push): add option to add plugin packages to solution (#57)

--- src/modules/dgt.power.push/Logic/AssemblyModelBuilder.cs
+++ src/modules/dgt.power.push/Logic/AssemblyModelBuilder.cs
@@ -72,6 +72,8 @@ internal class AssemblyModelBuilder
             result.Version = pluginPackage.Version!;
             result.Content = pluginPackage.Content!;
             result.Id = pluginPackage.PluginPackageId!.Value;
+
+            result.Solutions = GetSolutions(pluginPackage.ToEntityReference());
         }
 
         return result;
