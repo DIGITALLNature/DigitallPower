48d329e4c4c34c2432f6db6f0de1ad8f930b8ed9
Micha Oberstein <Micha.Oberstein@digitall.com>
2023-12-29 13:26:35 +0100

    fix(push): fix test dependencies

--- tests/dgt.power.push.tests/PushTest.cs
+++ tests/dgt.power.push.tests/PushTest.cs
@@ -2,11 +2,13 @@
 // DIGITALL Nature licenses this file to you under the Microsoft Public License.
 
 using dgt.power.push.Base;
+using dgt.power.push.Logic;
 using dgt.power.push.tests.Base;
 using dgt.power.tests;
 using dgt.power.tests.FakeExecutor;
 using FluentAssertions;
 using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit.Abstractions;
 
 namespace dgt.power.push.tests;
@@ -19,7 +21,11 @@ public class PushTest : PushTestsBase<PushCommand>
 
     protected override CommandTestContext<PushCommand, PushVerb> GetContext()
     {
+        var serviceCollection = new TestServiceCollection()
+            .AddScoped<WebresourcesProcessor>();
+
         return GetBuilder()
+            .WithServiceCollection(serviceCollection)
             .WithFakeMessageExecutor<AddSolutionComponentRequest>(new AddSolutionComponentExecutor())
             .Build();
     }
