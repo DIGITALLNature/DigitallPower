0c95f396013829175eca25db6c51b1cac92b38ea
Micha Oberstein <Moberstein@users.noreply.github.com>
2025-03-12 19:46:21 +0100

    fix: find customapi by messagename

--- src/modules/dgt.power.push/Logic/AssemblyModelBuilder.cs
+++ src/modules/dgt.power.push/Logic/AssemblyModelBuilder.cs
@@ -343,7 +343,7 @@ internal class AssemblyModelBuilder
 
                     if (customAttribute.AttributeType.Name == nameof(CustomApiRegistrationAttribute))
                     {
-                        type.CustomApi = GetValue<string>(customAttribute, "MessageName")!;
+                        type.CustomApi = GetValue<string>(customAttribute, "messageName")!;
                     }
                 }
 
